b2f797ce588e3ba06b37f4436cf7e6fb04c4b1dd
luisivmaraz <luisivmaraz03@gmail.com>
2024-04-14 10:25:50 -0600

    sec updt

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import { config } from "dotenv";
 config();
-import routerTickets from './routes/ticketsRoutes.js'; // Cambiado a las rutas de tickets
+import routerSensors from './routes/IoTRoutes.js'; // Cambiado a las rutas de tickets
 
 const app = express();
 
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
-app.use('/api/tickets', routerTickets); // Cambiado a utilizar las rutas de tickets
+app.use('/api/sensors', routerSensors); // Cambiado a utilizar las rutas de tickets
 
 export default app;
 
--- src/dao/IoTDao.js
+++ src/dao/IoTDao.js
@@ -1,16 +1,16 @@
 import Sensor from '../models/IoTModel.js'
 
 const C = console.log.bind(console.log)
-const ticketDAO = {}
+const IoTDao = {}
 
-ticketDAO.getAll = async () => {
+IoTDao.getAll = async () => {
     const sensors = await Sensor.find()
     return sensors
 }
 
 
 
-ticketDAO.insertTicket = async (ticket) => {
+IoTDao.InsertSensor = async (ticket) => {
     const sensorSaved = new Sensor(ticket)
     await sensorSaved.save()
     return true
